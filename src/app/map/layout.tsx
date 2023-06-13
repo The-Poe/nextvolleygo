836db241554265b869899c809676129c20ad3fce
AzureAD\frontend <frontend@holieninnovation.com.tw>
2023-06-13 16:09:16 +0800

    eslint setting

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -1,10 +1,3 @@
-
-export default function MapLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-      <>{children}</>
-  )
+export default function MapLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
